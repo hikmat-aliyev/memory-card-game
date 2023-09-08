6029be5547bcdc949ef3ceee54a67868a79d6c7f
Hikmat Ali <hikmat.aliyevm@gmail.com>
2023-09-08 16:17:38 +0400

    Create Start page component

--- src/components/RickAndMorty.jsx
+++ src/components/RickAndMorty.jsx
@@ -1,59 +1,80 @@
+
 import {useState, useEffect} from "react";
 import '../styles/RickAndMorty.css'
 
 export default function RickAndMorty() {
-    const [rickAndMorty, setRickAndMorty] = useState(null);
-    let charactersArray = [];
-    const [selectedCharacters, setSelectedCharacters] = useState(0);
-    const [selectedCharsArray, setSelectedCharsArray] = useState([]);
-    const [unselectedCharsArray, setUnselectedCharsArray] = useState([]);
+    const totalCharsNum = 5;
+   
+    const [allCharsArray, setAllCharsArray] = useState(null);
+    const [selectedCharsNum, setSelectedCharsNum] = useState(0);
+    const [shownUnselectedArray, setShownUnselectedArray] = useState(null);
+    const [shownSelectedArray, setShownSelectedArray] = useState(null);
+    const [selected, setSelected] = useState(false);
 
     useEffect(() => {
         fetch('https://rickandmortyapi.com/api/character')
             .then(response => response.json())
-            .then(json =>  setRickAndMorty(json.results));
+            .then(json => {
+                const resultArray = [...json.results];
+                filterAllCharsArray(resultArray);
+                filterShownUnselectedArray(resultArray);
+            });
     }, [])
-
-    const numberOfCharacters = 5;
-
-    if(rickAndMorty) {
-       charactersArray = rickAndMorty.slice(0, numberOfCharacters);
+    
+    function filterAllCharsArray(array) {
+        array.splice(5, 1);
+        array.splice(8, 4);
+        array.splice(10, 2);
+        array.splice(11, 1);
+        array.splice(totalCharsNum)
+        array.map((item, index) => {
+            item.status = 'Unselected'
+            item.id = index;
+        });
+        setAllCharsArray(array);
     }
 
-    let unselectedCharacters = numberOfCharacters - selectedCharacters;
-
-    function assignCharactersToArrays (updatedArray) {
-        const selected = updatedArray.filter(item => item.status === 'Selected');
-        const unselected = updatedArray.filter(item => item.status !== 'Selected');
-        setSelectedCharsArray(selected);
-        setUnselectedCharsArray(unselected);
+    function filterShownUnselectedArray() {
+        
     }
 
-    console.log(selectedCharsArray);
-    console.log(unselectedCharsArray);
-
-    function changeStatusOfCharacter(index) {
-        const updatedArray = [...charactersArray];
-        updatedArray[index].status = 'Selected';
-        setSelectedCharacters(selectedCharacters + 1);  
-        assignCharactersToArrays(updatedArray);
-        console.log(updatedArray);
+    function changeStatusOfChars(item) {
+        if(item.status !== 'Selected') {
+            setSelectedCharsNum(selectedCharsNum + 1);
+            item.status = 'Selected';
+        }else if(item.status === 'Selected'){
+            console.log('game over')
+        }
+        shuffleArray();
     }
 
+    if(selectedCharsNum === totalCharsNum) console.log('u win')
+
+    function shuffleArray() {
+        const newArray = [...allCharsArray]; // Create a copy of the array
+        for (let i = newArray.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1)); // Generate a random index
+            console.log(i, j);
+            [newArray[i], newArray[j]] = [newArray[j], newArray[i]]; // Swap elements
+        }
+        setAllCharsArray(newArray);
+    }
 
     return (
+
         <div>
-            {rickAndMorty && (
+            {allCharsArray && (
                 <div className="characters-container">
-                    {charactersArray.map((item, index) => (
-                        <div className="character-container" key={index} onClick={() => changeStatusOfCharacter(index)}>
-                             <img src={item.image} alt={item.name} />
-                             <h3>{item.name}</h3>
+                    {allCharsArray.map((item, index) => (
+                        <div className="character-container" key={index}>
+                             <img src={item.image} alt={item.name} onClick={() => changeStatusOfChars(item)}/>
+                             <h3>{item.name}</h3> 
                         </div>
-                        
                     ))}
                 </div>
-         )}
-        </div>    
+             )}
+
+        </div>  
+        
     )
 }
